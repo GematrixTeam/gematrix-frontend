8b877264cae3829ca33f29cca953364769988f42
andrey kazakov <aakazakovv@yandex.ru>
2019-10-11 18:54:24 +0300

    1.Get-request method added; 2.Air-bnb code-style formate

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-const app = new Vue ({
-    el: '#app',
-    methods:{}
+const app = new Vue({
+  el: '#app',
+  methods: {
+    async getDataFrom(path) {
+      try {
+        const result = await fetch(path);
+        return await result.json();
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
 });
