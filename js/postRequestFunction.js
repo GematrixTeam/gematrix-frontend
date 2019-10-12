5f05a1f886d57f4b07d7b7626b5451857194fb93
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2019-10-12 18:53:00 +0300

    New update function for post request

--- js/postRequestFunction.js
+++ js/postRequestFunction.js
@@ -1,19 +1,21 @@
-postData("http://example.com/answer", { answer: 42 })
-  .then(data => console.log(JSON.stringify(data))) // JSON-строка полученная после вызова `response.json()`
-  .catch(error => console.error(error));
-
-function postData(url = "", data = {}) {
-  return fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, cors, *same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json"
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrer: "no-referrer", // no-referrer, *client
-    body: JSON.stringify(data) // тип данных в body должен соответвовать значению заголовка "Content-Type"
-  }).then(response => response.json()); // парсит JSON ответ в Javascript объект
+async function postData(path = "", data = {}) {
+  try {
+    return result = await fetch(path, data);
+    //return result;
+  } catch (err) {
+    throw new Error(err);
+  }
 }
+
+postData('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo158',
+      body: 'bar573',
+      userId: 57
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  }).then(response => response.json())
+  .then(json => console.log(json))
