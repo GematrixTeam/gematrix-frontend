2a27b22aa0687459ec7e830b6e5af6c6b6d5ca86
andrey kazakov <aakazakovv@yandex.ru>
2019-10-14 11:12:47 +0300

    added get-request method

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,18 @@
-const app1 = new Vue ({
-    el: '#app-1',
-    methods:{}
+const app1 = new Vue({
+  el: '#app-1',
+  methods: {
+    async getDataFrom(path) {
+      try {
+        const result = await fetch(path);
+        return await result.json();
+      } catch (err) {
+        return `ERROR ${err}`;
+      }
+    },
+  },
 });
 
-const app2 = new Vue ({
-    el: '#app-2',
-    methods:{}
+const app2 = new Vue({
+  el: '#app-2',
+  methods: {},
 });
