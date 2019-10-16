fb7f23c1137635e25e6a58f2e73e492cba4d4211
andrey kazakov <aakazakovv@yandex.ru>
2019-10-16 13:39:25 +0300

    vue-project preparation

---
+++ gematrix/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+// сюда импортируем компоненты
+// ///////////////////////////
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+      /* например... */
+#app {
+  background-color: aliceblue;
+}
+</style>
