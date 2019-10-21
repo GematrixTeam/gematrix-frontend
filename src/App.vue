586c9d0356569be0c93932301d99c250a3f4f2ad
andrey kazakov <aakazakovv@yandex.ru>
2019-10-21 19:32:27 +0300

    added styles, components for landing page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
+    <div class="header-wrapper">
+      <header class="header">
+        <span class="header__text">GEMATRIX</span>
+      </header>
+    </div>
+    <div class="main">
+      <div class="main__left-part">
+        <TheSearch />
+        <DataList :datasets="datasets" />
+      </div>
+      <div class="main__right-part"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import TheSearch from '@/components/TheSearch.vue';
+import DataList from '@/components/DataList.vue';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      datasets: [
+        {
+          text: 'dskljfld ksdlfjskldfj  sdklfjsldkfjls sdfkljsdlkfjsldkfjldkjsdklfj dskfjdkfjdl 111',
+        },
+        {
+          text: 'dskljfld ksdlfjskldfj  sdklfjsldkfjls sdfkljsdlkfjsldkfjldkjsdklfj dskfjdkfjdl 222',
+        },
+        {
+          text: 'dskljfld ksdlfjskldfj  sdklfjsldkfjls sdfkljsdlkfjsldkfjldkjsdklfj dskfjdkfjdl 333',
+        },
+      ],
+    };
+  },
   components: {
-
+    TheSearch, DataList,
   },
 };
 </script>
 
-<style>
+<style lang="sass" scoped>
+  @import 'assets/style/sass/style.sass'
 </style>
---
+++ src/components/DataListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="data-list-item">
+    <b-card no-body class="overflow-hidden" style="max-width: 1144px;">
+      <b-row no-gutters>
+        <b-col md="6">
+          <b-card-body title="Example dataset name">
+            <b-card-text>
+              {{ dataset.text }}
+            </b-card-text>
+          </b-card-body>
+        </b-col>
+        <b-col md="6">
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+          <b-card-img src="https://via.placeholder.com/700x160" class="rounded-0"></b-card-img>
+        </b-col>
+      </b-row>
+  </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dataset: Object,
+  },
+};
+</script>
