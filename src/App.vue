1dfd7d80412055c91f64f451ba57ea3f260450e8
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2019-10-21 21:56:03 +0300

    bootstrap works

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    
+    <header>
+      <h1>Vue.js SPA</h1>
+    </header>
+
+    <b-breadcrumb :items="items"></b-breadcrumb>
+
+    <main>
+      <aside class="sidebar">
+      </aside>
+      <div class="content">
+      </div>
+    </main>
+
+
+
   </div>
 </template>
 
@@ -11,8 +25,31 @@ export default {
   components: {
 
   },
+  data() {
+    return {
+      items: [
+        {
+          text: 'Admin',
+          href: '#',
+        },
+        {
+          text: 'Manage',
+          href: '#',
+        },
+        {
+          text: 'Library',
+          active: true,
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+  h1 {
+    color: red;
+    text-align: center;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/style/custom.scss';
 import App from './App.vue';
 
 Vue.use(BootstrapVue);
