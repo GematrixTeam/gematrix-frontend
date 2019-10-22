6c604d8421f24488e9b99c7bd3fdfc64b2e83e9e
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2019-10-22 19:06:32 +0300

    Updated file structure

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import './assets/style/custom.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App.vue';
 
 Vue.use(BootstrapVue);
