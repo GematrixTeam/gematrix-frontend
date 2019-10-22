b21e0525e0fabd1df896a7c7fb80d4cc23ed54f1
andrey kazakov <aakazakovv@yandex.ru>
2019-10-22 17:47:54 +0300

    added loader on the landing page

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@
     <div class="main">
       <div class="main__left-part">
         <TheSearch />
-        <DataList :datasets="datasets" />
+        <TheLoader v-if="loading" />
+        <DataList v-else :datasets="datasets" />
       </div>
       <div class="main__right-part">
         <button class="main__add-data-btn">Add dataset</button>
@@ -21,21 +22,24 @@
 <script>
 import TheSearch from '@/components/TheSearch.vue';
 import DataList from '@/components/DataList.vue';
+import TheLoader from '@/components/TheLoader.vue';
 
 export default {
   name: 'app',
   data() {
     return {
       datasets: [],
+      loading: true,
     };
   },
   mounted() {
     this.getDataFrom('https://jsonplaceholder.typicode.com/posts?_limit=5').then((result) => {
       this.fillTheArrayWith(result);
+      this.loading = false;
     });
   },
   components: {
-    TheSearch, DataList,
+    TheSearch, DataList, TheLoader,
   },
   methods: {
     async getDataFrom(path) {
