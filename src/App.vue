5a40f2e4da66879e030f1cc4683ebb5f8da2cfbb
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2019-10-25 21:48:01 +0300

    Returned App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,16 @@
 <template>
   <div id="app">
-    <div class="header-wrapper">
-      <header class="header">
-        <span class="header__text">GEMATRIX</span>
-      </header>
-    </div>
-    <div class="main">
-      <div class="main__left-part">
-        <TheSearch />
-        <TheLoader v-if="loading" />
-        <DataList v-else :datasets="datasets" />
-      </div>
-      <div class="main__right-part">
-        <ButtonAddDataset  class="main__add-data-btn" />
-        <aside class="main__aside"></aside>
-      </div>
-    </div>
+    <ButtonAddDataset />
   </div>
 </template>
 
 <script>
-import TheSearch from '@/components/TheSearch.vue';
-import DataList from '@/components/DataList.vue';
-import TheLoader from '@/components/TheLoader.vue';
 import ButtonAddDataset from '@/components/ButtonAddDataset.vue';
+
 export default {
   name: 'app',
-  data() {
-    return {
-      datasets: [],
-      loading: true,
-    };
-  },
-  mounted() {
-    this.getDataFrom('https://jsonplaceholder.typicode.com/posts?_limit=5').then((result) => {
-      this.fillTheArrayWith(result);
-      this.loading = false;
-    });
-  },
   components: {
-    TheSearch, DataList, TheLoader, ButtonAddDataset,
-  },
-  methods: {
-    /**
-    * Method sends a get- request.
-    * @param {string} path - the path to...
-    * @return {array} - array of objects.
-    * @return {string} - error messege if catch some error.
-    */
-    async getDataFrom(path) {
-      try {
-        const result = await fetch(path);
-        return await result.json();
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-    /**
-    * Method fills the array with data.
-    * @param {array} data
-    */
-    fillTheArrayWith(data) {
-      this.datasets = [...data];
-    },
+    ButtonAddDataset,
   },
 };
 </script>
-
-<style lang="sass" scoped>
-  @import 'assets/style/sass/style.sass'
-</style>
