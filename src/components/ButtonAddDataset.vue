afad19134cdec99f5ec562749db95dfcbaa678d0
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2019-10-26 19:54:28 +0300

    Zoomed and centered the add data button.

--- src/components/ButtonAddDataset.vue
+++ src/components/ButtonAddDataset.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-button v-b-modal.modal-uploadPage>Add dataset</b-button>
+    <b-button v-b-modal.modal-uploadPage size="lg">Add dataset</b-button>
 
     <b-modal id="modal-uploadPage" size="lg" centered scrollable title="Add new dataset">
       <InputText titleField="Title" />
