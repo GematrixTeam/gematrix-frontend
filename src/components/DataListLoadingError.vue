d54cc0d7600b4dbc6af1bf54e38699e980fa1df7
aakazakov <48903729+aakazakov@users.noreply.github.com>
2019-11-03 02:01:56 +0300

    Set path feature (#15)
    * added var 'path' & commented out some methods (may be removed in the future)
    
    * added method to set request path
    
    * added configuration of get-request
    
    * added type='text/css'
    
    * corrected JSDoc
    
    * normalize.css added via npm
    
    * deleted comments
    
    * added error handling component

---
+++ src/components/DataListLoadingError.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="loading-error p-1 d-flex justify-content-center align-items-center flex-column">
+    <p><span class="loading-error__oops">Oops!</span>&#32;{{ error }}</p>
+    <b-img src="http://placekitten.com/400/500" fluid alt="Responsive image" class="py-1"></b-img>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: String,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+  .loading-error
+    font-family: Verdana, Geneva, Tahoma, sans-serif
+    font-size: large
+    &__oops
+      background-color: yellow
+</style>
