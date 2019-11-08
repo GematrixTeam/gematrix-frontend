c4107ee8fab3b0912ace9ad319965fc731a19caa
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-08 22:16:41 +0300

    trying to write tests

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -2,12 +2,22 @@ import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import App from '@/App.vue';
 
-describe('getting datasets', done => {
-  it('checking get-request method', () => {
-    const wrapper = shallowMount(App);
-    wrapper.vm.$nextTick(() => {
-      expect(wrapper.vm.datasets).to.not.be.empty;
-      done();
-    });
-  });
-});
+
+// describe('getting datasets', () => {
+//   it('checking get-request method', done => {
+//     const wrapper = shallowMount(App);
+//     wrapper.vm.$_getDataFrom('../tests/testData.json', {method: 'GET'})
+//     .then((res) => {console.log(`Oops: ${res}`); done();});
+//   });
+// });
+
+// describe('getting datasets', () => {
+//   it('checking get-request method', done => {
+//     const wrapper = shallowMount(App);
+//     wrapper.vm.$_getDataFrom('https://jsonplaceholder.typicode.com/posts?_limit=5', {method: 'GET'});
+//     wrapper.vm.$nextTick(() => {
+//       expect(wrapper.vm.datasets).to.not.be.empty;
+//       done();
+//     });
+//   });
+// });
