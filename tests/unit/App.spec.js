ba63f2588fe5a1e4f3fa7186dacc7d1f33dd84fa
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-09 22:23:07 +0300

    wrote test 'we have the Get-request path'

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -3,9 +3,14 @@ import { shallowMount } from '@vue/test-utils';
 import App from '@/App.vue';
 
 
-describe('getting datasets', () => {
+describe('we have the Get-request path', () => {
   it('is there the path', () => {
     const wrapper = shallowMount(App);
     expect(wrapper.vm.requestPath).to.not.be.empty;
+  });
+
+  it('the path is string', () => {
+    const wrapper = shallowMount(App);
+    expect(typeof (wrapper.vm.requestPath) === 'string').to.be.true;
   });
 });
