3d94ffb07116305a88b5fc478bd15b839694704d
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-10 17:38:07 +0300

    wrote cheking get-req-method test

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -13,3 +13,14 @@ describe('we have the Get-request path', () => {
     expect(typeof (wrapper.vm.requestPath) === 'string').to.be.true;
   });
 });
+
+describe('$_getDataFrom() works correctly', () => {
+  it('we have got a valid response', done => {
+    return wrapper.vm.$nextTick(() => {
+      wrapper.vm.$_getDataFrom('https://jsonplaceholder.typicode.com/posts?_limit=5', {method: 'GET'}).then((res) => {
+        expect(res).to.have.lengthOf(5);
+        done();
+      }).catch(done);
+    });
+  });
+});
