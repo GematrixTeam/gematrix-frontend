9ef136c060c41df8b332529718b52025f9f75d21
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-11 09:43:25 +0300

    code corrected alittle

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -8,36 +8,35 @@ const wrapper = shallowMount(App, {
     'b-container': BContainer,
     'b-col': BCol,
     'b-row': BRow,
-  }
+  },
 });
 
-describe('we have the Get-request path', () => {
-  it('is there the path', () => {
-    const path = 'this is the right path';
-    wrapper.vm.$_setRequestPath(path);
-    expect(wrapper.vm.requestPath === path).to.be.true;
-  });
-  it('the path is string', () => {
-    expect(wrapper.vm.requestPath).to.be.a('string');
-  });
-});
+// describe('we have the Get-request path', () => {
+//   it('is there the path', () => {
+//     const path = 'this is the right path';
+//     wrapper.vm.$_setRequestPath(path);
+//     expect(wrapper.vm.requestPath === path).to.be.true;
+//   });
+//   it('the path is string', () => {
+//     expect(wrapper.vm.requestPath).to.be.a('string');
+//   });
+// });
 
 describe('$_getDataFrom(...) works', () => {
   it('we have got a valid response', (done) => {
-    return wrapper.vm.$nextTick(() => {
-      wrapper.vm.$_getDataFrom('https://jsonplaceholder.typicode.com/posts?_limit=5', {method: 'GET'}).then((res) => {
-        expect(res).to.have.lengthOf(5);
+    wrapper.vm.$_getDataFrom('https://jsonplaceholder.typicode.com/posts?_limit=3', { method: 'GET' })
+      .then((res) => {
+        expect(res).to.have.lengthOf(3);
         done();
       }).catch(done);
-    });
   });
 });
 
-describe('$_fillTheArrayWith(...) works', () => {
-  it('method should fill datasets property', () => {
-    const arr = [1, 'a', '#', true, {}, []];
-    wrapper.vm.$_fillTheArrayWith(arr);
-    expect(wrapper.vm.datasets).to.have.lengthOf(arr.length);
-    expect(wrapper.vm.datasets).to.include(...arr);
-  });
-});
+// describe('$_fillTheArrayWith(...) works', () => {
+//   it('method should fill datasets property', () => {
+//     const arr = [1, 'a', '#', true, {}, []];
+//     wrapper.vm.$_fillTheArrayWith(arr);
+//     expect(wrapper.vm.datasets).to.have.lengthOf(arr.length);
+//     expect(wrapper.vm.datasets).to.include(...arr);
+//   });
+// });
