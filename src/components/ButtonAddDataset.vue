e61ec5a6d5f36bf53c2648692e21c92c1849243d
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2019-12-21 16:40:04 +0300

    Returns the previous value

--- src/components/ButtonAddDataset.vue
+++ src/components/ButtonAddDataset.vue
@@ -14,7 +14,7 @@
       <b-form ref="my-form"
       @submit="onSubmit"
       @reset="onReset"
-      action="http://gematrix.hntr.info/api/v1/datasets/"
+      action="https://jsonplaceholder.typicode.com/posts"
       method="post"
       v-if="show">
         <b-form-group id="input-group-1" label="Title:" label-for="input-1">
